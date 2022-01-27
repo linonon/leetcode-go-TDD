cfae9a459d83cadff364b846f9be28ad15ac4c4f
Linonon <an493454270@gmail.com>
2022-01-27 14:41:17 +0800

    ✨ feat(Init 2047):

---
+++ code/unknow/2047-countValidWords/2047.go
@@ -0,0 +1,14 @@
+package leetcode
+
+/*
+ * @lc app=leetcode.cn id=2047 lang=golang
+ *
+ * [2047] 句子中的有效单词数
+ */
+
+// @lc code=start
+func countValidWords(sentence string) int {
+
+}
+
+// @lc code=end
