615c5aef0f3acb5851aad6fff553d671076cd3f3
Linonon <an493454270@gmail.com>
2022-01-28 13:37:09 +0800

    Finshed 1996

--- code/algorithms/1996-numberOfWeakCharacters/1996.go
+++ code/algorithms/1996-numberOfWeakCharacters/1996.go
@@ -1,5 +1,9 @@
 package leetcode
 
+import "sort"
+
+// import "sync"
+
 /*
  * @lc app=leetcode.cn id=1996 lang=golang
  *
@@ -8,7 +12,20 @@ package leetcode
 
 // @lc code=start
 func numberOfWeakCharacters(properties [][]int) int {
-
+	result := 0
+	sort.Slice(properties, func(i, j int) bool {
+		p, q := properties[i], properties[j]
+		return p[0] > q[0] || (p[0] == q[0] && p[1] < q[1])
+	})
+	maxDef := 0
+	for _, p := range properties {
+		if p[1] < maxDef {
+			result++
+		} else {
+			maxDef = p[1]
+		}
+	}
+	return result
 }
 
 // @lc code=end
