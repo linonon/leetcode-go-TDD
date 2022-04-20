c7d029dbfd2109a5028b656f3e3664c1fe5c8413
Linonon <an493454270@gmail.com>
2022-04-20 18:20:38 +0800

    定時提交: 2022-04-20 18:20:38

--- code/algorithms/2024/2024.go
+++ code/algorithms/2024/2024.go
@@ -8,33 +8,33 @@ package leetcode
 
 // @lc code=start
 
-type word struct {
-	wLen     int
-	distance int
-	next     *word
+func maxConsecutiveChar(answerKey string, k int, ch byte) (ans int) {
+	left, sum := 0, 0
+	for right := range answerKey {
+		if answerKey[right] != ch {
+			sum++
+		}
+		for sum > k {
+			if answerKey[left] != ch {
+				sum--
+			}
+			left++
+		}
+		ans = max(ans, right-left+1)
+	}
+	return
 }
 
-func maxConsecutiveAnswers(answerKey string, k int) (result int) {
+func maxConsecutiveAnswers(answerKey string, k int) int {
+	return max(maxConsecutiveChar(answerKey, k, 'T'),
+		maxConsecutiveChar(answerKey, k, 'F'))
+}
 
-	var t, f = &word{}, &word{}
-	var dumt, dumf = &word{}, &word{}
-	dumt.next = t
-	dumt.next = f
-	for i := 0; i < len(answerKey); i++ {
-		if answerKey[i] == 'T' {
-			t.wLen++
-		}
-		for ; i < len(answerKey) && answerKey[i] != 'T'; i++ {
-			t.distance++
-		}
-		if i >= len(answerKey) {
-			break
-		}
-		t.next = &word{}
-		t = t.next
+func max(a, b int) int {
+	if b > a {
+		return b
 	}
-
-	return
+	return a
 }
 
 // @lc code=end
