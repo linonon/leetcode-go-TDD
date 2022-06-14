8695d201e7f6aa0f3e79ce6e96e34e6579a4b5d8
oliver <oliver@gmail.com>
2022-06-14 18:20:31 +0800

    定時提交: 2022-06-14 18:20:31

--- code/algorithms/498/498.go
+++ code/algorithms/498/498.go
@@ -8,7 +8,45 @@ package leetcode
 
 // @lc code=start
 func findDiagonalOrder(mat [][]int) []int {
+	var (
+		m           = len(mat)
+		n           = len(mat[0])
+		result      = make([]int, 0, m*n)
+		x, y    int = 0, 0
+		reverse bool
+		count   int
+	)
+	for y < m && x < n && count != n*m {
+		result = append(result, mat[y][x])
+		if !reverse {
+			x++
+			y--
+			if y < 0 {
+				y = 0
+				reverse = true
+			}
+			if x == n {
+				y++
+				x = n - 1
+				reverse = true
+			}
+		} else {
+			x--
+			y++
+			if x < 0 {
+				x = 0
+				reverse = false
+			}
+			if y == m {
+				x += 2
+				y = m - 1
+				reverse = false
+			}
+		}
+		count++
+	}
 
+	return result
 }
 
 // @lc code=end
--- code/algorithms/498/498_test.go
+++ code/algorithms/498/498_test.go
@@ -15,11 +15,25 @@ func Test_findDiagonalOrder(t *testing.T) {
 		want []int
 	}{
 		{
-			name: "1",
+			name: "[[1,2],[3,4]]",
 			args: args{
 				[][]int{{1, 2}, {3, 4}},
 			},
 			want: []int{1, 2, 3, 4},
+		},
+		{
+			name: "[[1,2,3],[4,5,6],[7,8,9]]",
+			args: args{
+				[][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+			},
+			want: []int{1, 2, 4, 7, 5, 3, 6, 8, 9},
+		},
+		{
+			name: "[[1,2],[3,4],[5,6]",
+			args: args{
+				[][]int{{1, 2}, {4, 5}, {7, 8}},
+			},
+			want: []int{1, 2, 4, 7, 5, 8},
 		},
 		// TODO: Add test cases.
 	}
